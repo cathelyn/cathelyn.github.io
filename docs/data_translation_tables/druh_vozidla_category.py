bf375c3917ad4321a5930a679adc9053115040a5
Katarina Bulkova <kbulkova@redhat.com>
2022-12-01 21:52:04 +0100

    Data merging: one record per accident
    Merge data based on common columns subset (mainly location and exact
    time), group different values into lists.
    
    Update translation tables.
    
    Signed-off-by: Katarina Bulkova <kbulkova@redhat.com>

---
+++ docs/data_translation_tables/druh_vozidla_category.py
@@ -0,0 +1,23 @@
+druh_vozidla_category = {
+    'autobus': 'bus',
+    'chodec': 'pedestrian',
+    'jiné motorové vozidlo (zemědělské, lesní, stavební stroje atd.)': 'other motor vehicle (agricultural, forestry, construction machinery, etc.)',
+    'jiné nemotorové vozidlo': 'other non-motor vehicle',
+    'jiný druh vozidl': 'other vehicle type',
+    'jízdní kolo': 'bicycle',
+    'malý motocykl (do 50 ccm)': 'small motorcycle (up to 50 cc)',
+    'moped': 'moped',
+    'motocykl (včetně sidecarů, skútrů apod.)': 'motorcycle (including sidecars, scooters, etc.)',
+    'nezjištěno, řidič ujel': 'unknown, driver left the scene',
+    'nákladní automobil (včetně multikáry, autojeřábu, cisterny atd.)': 'truck (including multicar, truck crane, tanker, etc.)',
+    'nákladní automobil s návěsem': 'truck with semi-trailer',
+    'nákladní automobil s přívěsem': 'truck with trailer',
+    'osobní automobil bez přívěsu': 'car with without trailer',
+    'osobní automobil s přívěsem': 'car with trailer',
+    'povoz, jízda na koni': 'carriage, horse riding',
+    'traktor (i s přívěsem)': 'tractor (also with trailer)',
+    'tramvaj': 'tram',
+    'trolejbus': 'trolley',
+    'vlak': 'train',
+
+}
